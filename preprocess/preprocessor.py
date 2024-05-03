471994bd3500b6632a7d56fa454213c49ffe3222
lmBored <baokhoi136@gmail.com>
2024-05-03 22:08:56 +0200

    Catch unexpected error

--- preprocess/preprocessor.py
+++ preprocess/preprocessor.py
@@ -10,29 +10,33 @@ def text_transformer(text):
     return text
 
 def preprocessor(tweet):
-    text = ''
-    if 'extended_tweet' in tweet:
-        text = tweet['extended_tweet']['full_text']
-    elif 'text' in tweet:
-        text = tweet['text']
+    try:
+        text = ''
+        if 'extended_tweet' in tweet:
+            text = tweet['extended_tweet']['full_text']
+        elif 'text' in tweet:
+            text = tweet['text']
+                
+        text = text_transformer(text)
+                
+        if 'delete' not in tweet:
+            tweets_info = {k:v for k,v in tweet.items() if k in tweets_keys}
+            users_info = {k:v for k,v in tweet['user'].items() if k in users_keys}
+            users_info['user_id'] = users_info.pop('id')
+            tweets_info.update(users_info)
             
-    text = text_transformer(text)
-            
-    if 'delete' not in tweet:
-        tweets_info = {k:v for k,v in tweet.items() if k in tweets_keys}
-        users_info = {k:v for k,v in tweet['user'].items() if k in users_keys}
-        users_info['user_id'] = users_info.pop('id')
-        tweets_info.update(users_info)
-        
-        airlines_mentioned = [airline for airline in airlines_list if airline in text.lower()] # Need to also consider changing language
-        mentioned_id = [i['id'] for i in tweet['entities']['user_mentions']]
-        # text = re.sub(r'@[^ ]+', '', text) # remove username
-        extended_tweets = {'text':text, 'language':tweet['lang'], 'mentioned_airlines':airlines_mentioned, 'user_mentions':mentioned_id}
-        tweets_info.update(extended_tweets)
-
-        nullables = ['in_reply_to_status_id', 'in_reply_to_user_id', 'coordinates', 'place']
-        for i in nullables:
-            if tweets_info[i] == None:
-                tweets_info[i] = 'NULL'
-
-        return tweets_info
+            airlines_mentioned = [airline for airline in airlines_list if airline in text.lower()] # Need to also consider changing language
+            mentioned_id = [i['id'] for i in tweet['entities']['user_mentions']]
+            # text = re.sub(r'@[^ ]+', '', text) # remove username
+            extended_tweets = {'text':text, 'language':tweet['lang'], 'mentioned_airlines':airlines_mentioned, 'user_mentions':mentioned_id}
+            tweets_info.update(extended_tweets)
+    
+            nullables = ['in_reply_to_status_id', 'in_reply_to_user_id', 'coordinates', 'place']
+            for i in nullables:
+                if tweets_info[i] == None:
+                    tweets_info[i] = 'NULL'
+    
+            return tweets_info
+    except Exception as e:
+        print(f"Error: {e}")
+        return None

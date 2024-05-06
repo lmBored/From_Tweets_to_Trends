e103a4d2733cb2b97bc76b805bfb9abe08c62e51
jangalic04 <j.a.galic@student.tue.nl>
2024-05-06 13:29:47 +0200

    In main.py on line 3 '=' was missing. In requirements.txt file the version of ipython module was not available.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from preprocess import preprocessor, loader, initializer
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     data = [Path("data/"+file) for file in os.listdir('data')]
 
     connection = mysql.connector.connect(host=config.get('HOST'), user=config.get('USERNAME'), password=config.get('PASSWORD'),database=config.get('DATABASE'), allow_local_infile=True)    

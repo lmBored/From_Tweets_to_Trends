aab3762df1251763522832eddc31a26a2f80f40c
Nguyen Hoang Bao Khoi, Khoi <h.b.k.nguyen.hoang.bao.khoi@student.tue.nl>
2024-05-30 12:50:14 +0200

    fix conversation variable airlien

--- preprocess/conversation.py
+++ preprocess/conversation.py
@@ -18,20 +18,19 @@ def conversation_adder(connection):
     tweets = cursor.fetchall()
     print("Done fetching tweets!")
     
-    airlines = {"KLM": 56377143 ,
-            "AirFrance": 106062176 ,
-            "British_Airways": 18332190 ,
-            "AmericanAir": 22536055 ,
-            "Lufthansa": 124476322 ,
-            "AirBerlin": 26223583 ,
-            "AirBerlin assist": 2182373406 ,
-            "easyJet": 38676903 ,
-            "RyanAir": 1542862735 ,
-            "SingaporeAir": 253340062 ,
-            "Qantas": 218730857 ,
-            "EtihadAirways": 45621423 ,
-            "VirginAtlantic": 20626359
-            }
+    airlines= {56377143 : 'KLM',
+            106062176:'AirFrance',
+            18332190:"British_Airways", 
+            22536055:"AmericanAir",
+            124476322:"Lufthansa",
+            26223583:'AirBerlin',
+            2182373406:'AirBerlin assist',
+            38676903:"easyJet",
+            1542862735:"RyanAir",
+            253340062:"SingaporeAir",
+            218730857:"Qantas",
+            45621423:"EtihadAirways",
+            20626359:"VirginAtlantic"}
     
     n = len(tweets)
 
@@ -88,7 +87,7 @@ def conversation_adder(connection):
             else:
                 member_of[reply_id] = convs
                 
-        if counter % round(n/10) == 0:
+        if counter % round(n/100) == 0:
             try:
                 connection.commit()
             except Exception as e:

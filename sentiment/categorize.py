436a779fd1a1f4fcba4a7b7762725af58b5a75b1
Nguyen Hoang Bao Khoi, Khoi <h.b.k.nguyen.hoang.bao.khoi@student.tue.nl>
2024-06-03 01:54:40 +0200

    adjust categories
    Former-commit-id: f2975ae32a0ba5054b750aebe1636bb2964f1a48

--- sentiment/categorize.py
+++ sentiment/categorize.py
@@ -2,23 +2,39 @@ def categorize(connection):
     cursor = connection.cursor()
 
     categories = {
-        "baggage": ["baggage", "bagage", "bagagge", "baggae", "bagging", "handbag",
-        "in/bag", "balostmybag", "bag-drop", "luggage", "check-in/bag",
-        "bagport", "lostbag", "cabinbag", "bagggage", "bagagge", "luggage",
-        "lugggage", "lugage", "lugagge", "suitcase", "suitcsse", "carry on",
-        "carry-on", "carryon", "checked bag", "checked-ba", "backpack"],
-        
-        "delay_and_cancellation": ["dely", "dealy", "deleyed", "postpone", "postpon", "pospnoe", "cancel",
-        "cancelled", "canceled", "stuck"],
-        
-        "staff": ["staff", "employee", "employes", "personel", "crew", "worker", "pilot",
-        "piloot", "steward"],
-
-        "security_and_safety": ["secure", "security", "safe", "safety", "safely", "emergency", "incident",
-        "emergency landing", "accident", "burning"],
-        
-        "money": ["money", "refund", "compensation", "claim", "pay", "paid", "price",
-        "expensive", "cheap"]
+    "baggage": [
+        "baggage", "bagage", "bagagge", "baggae", "bagging", "handbag", 
+        "in/bag", "balostmybag", "bag-drop", "luggage", "check-in/bag", 
+        "bagport", "lostbag", "cabinbag", "bagggage", "bagagge", "luggage", 
+        "lugggage", "lugage", "lugagge", "suitcase", "suitcsse", "carry on", 
+        "carry-on", "carryon", "checked bag", "checked-ba", "backpack"
+    ],
+    
+    "delay_and_cancellation": [
+        "delay", "planedelays", "delayedflight", "flightdelayed", "delays", 
+        "delayed", "dely", "dealy", "deleyed", "postpone", "postponed", 
+        "postpones", "postpon", "pospnoe", "cancel", "cancelled", "canceled", 
+        "cancellation", "cancellations", "stuck"
+    ],
+    
+    "staff": [
+        "staff", "employee", "employes", "employees", "personel", "crew", 
+        "worker", "pilot", "piloot", "steward", "stewardess", "stewardes", 
+        "flight attendant", "flightattendant", "flightattendent", 
+        "flight-attendant", "attendant", "attendent", "host"
+    ],
+    
+    "security_and_safety": [
+        "secure", "securty", "security", "safe", "save", "savely", "safety", 
+        "safely", "emergency", "incident", "emergency landing", "accident", 
+        "burning", "fire", "burn", "panic"
+    ],
+    
+    "money": [
+        "money", "refund", "compensation", "compensatoin", "klaim", "claim", 
+        "pay", "pays", "afford", "affort", "paid", "price", "expensive", 
+        "cheap", "cheep", "cheaper"
+    ]
     }
 
     category_names = set(categories.keys())

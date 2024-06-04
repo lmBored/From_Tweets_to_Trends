c72b2c7a8f1783dd69011e856bc3a3fd3da6bd26
Nguyen Hoang Bao Khoi, Khoi <h.b.k.nguyen.hoang.bao.khoi@student.tue.nl>
2024-06-04 23:03:42 +0200

    add comments to main
    Former-commit-id: df27815efbe19b28a2ce88ea395878b15060c793

--- join_dataset.py
+++ join_dataset.py
@@ -11,3 +11,5 @@ file7 = pd.read_csv('tweets_dataset_sven2.csv')
 combined = pd.concat([file1, file2, file3, file4, file5, file6, file7])
 
 combined.to_csv('combined_dataset.csv', index=False)
+
+# cat tweets_dataset_khoi.csv <(tail -n +2 tweets_dataset_illija.csv) <(tail -n +2 tweets_dataset_sven.csv) > combined.csv

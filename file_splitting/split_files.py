e1c4397200f27efe799c534d76b0b21ec718cc7c
Nguyen Hoang Bao Khoi, Khoi <h.b.k.nguyen.hoang.bao.khoi@student.tue.nl>
2024-06-05 14:46:01 +0200

    remove backslash, update file splitting based on os and sentiment analyssi

--- file_splitting/split_files.py
+++ file_splitting/split_files.py
@@ -8,6 +8,11 @@ from pathlib import Path
 data = [Path("data/"+file) for file in os.listdir('data')]
 data.sort(key=lambda x: x.name)
 
-with open('json_files.txt', 'w') as f:
-    f.write('\n'.join(str(file).replace('data/', '') for file in data))
+if os.name == 'nt':  # Windows
+    with open('json_files.txt', 'w') as f:
+        f.write('\n'.join(str(file).replace('data\\', '') for file in data))
+else:  # Other operating systems
+    with open('json_files.txt', 'w') as f:
+        f.write('\n'.join(str(file).replace('data/', '') for file in data))
+    
 print("Files written to json_files.txt.")
